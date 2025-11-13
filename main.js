70eaf13dfae23818ee70db351b087f637c425f14
wcolonandre-ship-it <wcolonandre@gmail.com>
2025-11-13 01:02:17 +0000

    Agrego nuevo archivo

--- main.js
+++ main.js
@@ -17,8 +17,32 @@ Escribir un programa que pregunte al usuario si quiere una pizza vegetariana o n
 export const hacer_pedido_pizza = (tipo_pizza, ingrediente_extra) => {
     let resumen = ''
 
-    // codigo aqui
-    
+    console.log("Bienvenido a la pizzería Bella Napoli")
+    console.log("Qué tipo de pizza desea: 'vegetariana' o 'no vegetariana'")
+
+    if (tipo_pizza == "vegetariana"){ 
+        console.log("Qué ingrediente desea: Pimiento o Tofu")
+        if (ingrediente_extra == "Pimiento"){
+            resumen = "La pizza elegida es vegetariana y lleva Mozzarella, Tomate y Pimiento."
+        } else if (ingrediente_extra == "Tofu"){
+            resumen = "La pizza elegida es vegetariana y lleva Mozzarella, Tomate y Tofu."
+        } else {
+            resumen = "Error"
+        }
+
+    } else if (tipo_pizza == "no vegetariana"){
+        console.log("Qué ingrediente desea: Peperoni o Salmón")
+        if (ingrediente_extra == "Peperoni"){
+            resumen = "La pizza elegida es no vegetariana y lleva Mozzarella, Tomate y Peperoni."
+        } else if (ingrediente_extra == "Salmón"){
+            resumen = "La pizza elegida es no vegetariana y lleva Mozzarella, Tomate y Salmón."
+        } else {
+            resumen ="Error"
+        }
+
+    } else {
+        resumen = "Error"
+    }
 
     return resumen
 }
